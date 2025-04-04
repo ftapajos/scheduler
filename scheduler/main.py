69ede19b79fca0834400e26fb8fec10ff813be5f
Flávio Tapajós <tapajos@outlook.com>
2025-04-04 15:58:50 -0300

    refactor: create packages

---
+++ scheduler/main.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+import subprocess
+from datetime import datetime, timedelta, timezone
+from math import exp
+from sys import argv
+
+from tasklib import TaskWarrior
+
+from .utils import (
+    calculate_tag_sum,
+    extract_tags_from,
+    get_duration_on,
+    get_shares,
+    get_total_time_tags,
+    last_activity_time,
+    print_task,
+    tags_and_description,
+)
+
+tagless = "TAGLESSTASK"
+force_avoided_task_for_seconds = 25 * 60
+force_switch_after_seconds = 25 * 60
+
+
+def next():
+    # Apply custom filters and restrict to unblocked and pending tasks
+    tw = TaskWarrior()
+    filterString = " ".join(argv[1:])
+    if len(filterString) > 1:
+        filterString = "(" + filterString + ") "
+    else:
+        filterString = ""
+    filterString += "+UNBLOCKED and +PENDING"
+
+    # Gets context's filter and apply it manually
+    context = tw.execute_command(["_get", "rc.context"])[0]
+
+    if context:
+        context_read = tw.execute_command(["_get", "rc.context." + context + ".read"])[
+            0
+        ]
+        filterString += " and ( " + context_read + ")"
+
+    tasks = tw.tasks.filter(filterString)
+
+    # Check if there any tags
+    if len(tasks) <= 0:
+        print("Taskless")
+        subprocess.run(["task", "ls", filterString])
+        quit()
+
+    # Get all tags
+    tags = set([tag for task in tasks for tag in extract_tags_from(task)])
+
+    # Calculate virtual times
+    virtualTime = {}
+    tDict = {}
+    _tasksTags = []
+    for task in sorted(tasks, key=lambda d: d["urgency"], reverse=True):
+        _tags = set(tags_and_description(task))
+        if _tags not in _tasksTags:
+            _tasksTags.append(_tags)
+            tDict[task["id"]] = task
+            virtualTime[task["id"]] = exp(task["urgency"])
+
+    # Calculate executed times
+    executedTime = {
+        tid: get_duration_on(tags_and_description(tDict[tid]))
+        for tid in virtualTime.keys()
+    }
+
+    executedTimeTag = {tag: get_duration_on([tag]) for tag in tags}
+
+    totalTimeTags = get_total_time_tags(executedTimeTag.keys())
+    totalExecutedTime = sum(executedTime.values())
+
+    # Shows must urgent task, since there is no sample time
+    if totalTimeTags == 0 and totalExecutedTime == 0:
+        print("No sampled time")
+        print("Start by the most urgent task")
+        tasks = tw.tasks.filter(filterString)
+        print_task(sorted(tasks, key=lambda d: d["urgency"], reverse=True)[0])
+        quit()
+
+    totalVirtualTime = sum(virtualTime.values())
+
+    # Calculate virtual times for each tag
+    virtualTimeTag = calculate_tag_sum(tags, tDict, virtualTime)
+    sharesTag, executedSharesTag = get_shares(
+        executedTimeTag, virtualTimeTag, totalTimeTags, totalVirtualTime
+    )
+
+    tag_correction = {}
+    for tag in sharesTag.keys():
+        if executedSharesTag[tag] > sharesTag[tag]:
+            tag_correction[tag] = sharesTag[tag] / executedSharesTag[tag]
+        else:
+            tag_correction[tag] = 1
+
+    # Calculate executed time for each tag
+    tids = tDict.keys()
+    for tid in tids:
+        for tag in extract_tags_from(tDict[tid]):
+            virtualTime[tid] *= tag_correction[tag]
+
+    totalVirtualTime = sum(virtualTime.values())
+
+    # Calculate shares
+    shares, executedShares = get_shares(
+        executedTime, virtualTime, totalExecutedTime, totalVirtualTime
+    )
+
+    # Problem of a urgent long task and short tags
+    if 1 in executedShares.values():
+        tid = list(executedShares.keys())[list(executedShares.values()).index(1)]
+
+        if sorted(tasks, key=lambda d: d["urgency"], reverse=True)[0]["id"] == tid:
+            tags = tags_and_description(tDict[tid])
+            _last_activity_time = last_activity_time(tags)
+
+            if _last_activity_time < force_avoided_task_for_seconds:
+                print("This is the only pending task started today")
+                print(
+                    "Please keep doing it for",
+                    f"{int((
+                        force_avoided_task_for_seconds-_last_activity_time
+                      )/60)}min",
+                    "uninterruptly",
+                )
+                print_task(tDict[tid])
+                quit()
+
+    difference = {key: shares[key] - executedShares[key] for key in shares.keys()}
+
+    tid = max(difference, key=difference.get)
+
+    # Verify if 
+    _last_activity_time = last_activity_time(tags_and_description(tDict[tid]))
+    if _last_activity_time > force_switch_after_seconds and len(tasks) > 1:
+        skiped_task = tDict[tid]
+        print("skipping", skiped_task)
+        difference.pop(tid)
+        tid = max(difference, key=difference.get)
+
+    print("Needed: ", int(shares[tid] * 10000) / 100, "%", sep="")
+    print(
+        "Executed:  ",
+        (
+            int(executedTime[tid] * 10000 / totalExecutedTime) / 100
+            if totalExecutedTime > 0
+            else 0
+        ),
+        "%",
+        sep="",
+    )
+
+    print_task(tDict[tid])
---
+++ scheduler/utils.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+import json
+import subprocess
+import time
+from datetime import UTC, datetime, timedelta, timezone
+from math import exp
+from sys import argv
+
+from tasklib import TaskWarrior
+
+tagless = "TAGLESSTASK"
+force_avoided_task_for_seconds = 25 * 60
+force_switch_after_seconds = 25 * 60
+
+
+def extract_tags_from(task):
+    tags = []
+
+    # Extract attributes for use as tags.
+    if task["project"] is not None:
+        tags.append(task["project"])
+
+    if task["tags"] is not None:
+        if type(task["tags"]) is str:
+            # Usage of tasklib (e.g. in taskpirate) converts the tag list into
+            # a string
+            # If this is the case, convert it back into a list first
+            # See https://github.com/tbabej/taskpirate/issues/11
+            tags.extend(task["tags"].split(","))
+        else:
+            tags.extend(task["tags"])
+
+    if len(tags) == 0:
+        tags.append(tagless)
+    return tags
+
+
+def tags_and_description(task):
+    tags = extract_tags_from(task)
+    if len(tags) == 1 and tags[0] == tagless:
+        return [task["description"]]
+    else:
+        return [task["description"]] + tags
+
+
+def calculate_tag_sum(tags, taskDictionary, dictionary):
+    tagSum = {}
+    tids = taskDictionary.keys()
+    for tag in tags:
+        tagSum[tag] = sum(
+            [dictionary[tid] for tid in tids if tag in extract_tags_from(taskDictionary[tid])]
+        )
+    return tagSum
+
+
+def get_time_tw(event):
+    start = datetime.strptime(event["start"], "%Y%m%dT%H%M%SZ").replace(
+        tzinfo=timezone.utc
+    )
+    if "end" in event:
+        end = datetime.strptime(event["end"], "%Y%m%dT%H%M%SZ").replace(
+            tzinfo=timezone.utc
+        )
+    else:
+        end = datetime.now(UTC)
+    return (end - start).total_seconds()
+
+
+def get_total_time_tags(tags):
+    day = json.loads(subprocess.check_output(["timew", "export", ":day"]))
+
+    total_time = 0
+    for event in day:
+        if set(tags).intersection(set(event["tags"])):
+            total_time += get_time_tw(event)
+    return total_time
+
+
+def last_activity_time(tags):
+    day = json.loads(subprocess.check_output(["timew", "export", ":day"]))
+
+    last_activity = [event for event in day if event["id"] == 1][0]
+    if set(tags) == set(last_activity["tags"]):
+        return get_time_tw(last_activity)
+    else:
+        return 0
+
+
+def print_task(task):
+    day = json.loads(subprocess.check_output(["timew", "export"]))
+
+    if task["start"] is not None:
+        tw_ids = [
+            d["id"] for d in day if set(d["tags"]) == set(tags_and_description(task))
+        ]
+
+        if len(tw_ids) > 0:
+            tw_id = min(tw_ids)
+            print(f"Timewarrior id: @{tw_id}")
+
+    subprocess.run(["task", "ls", str(task["id"])])
+
+
+def get_duration_on(tags):
+    if tagless in tags:
+        tags.remove(tagless)
+
+    if len(tags) <= 0:
+        return 0
+
+    day = json.loads(subprocess.check_output(["timew", "export", ":day"]))
+
+    total_time = 0
+    tags = set(tags)
+    for event in day:
+        if tags.intersection(set(event["tags"])) == tags:
+            total_time += get_time_tw(event)
+    return total_time
+
+
+def get_shares(executedTime, virtualTime, totalExecutedTime, totalVirtualTime):
+    assert len(executedTime) == len(virtualTime)
+    assert executedTime.keys() == virtualTime.keys()
+
+    keys = executedTime.keys()
+    # Calculate executed shares
+    executedShares = {
+        key: (executedTime[key] / totalExecutedTime if totalExecutedTime > 0 else 0)
+        for key in keys
+    }
+
+    # Calculate shares
+    shares = {key: virtualTime[key] / totalVirtualTime for key in keys}
+    return (shares, executedShares)
